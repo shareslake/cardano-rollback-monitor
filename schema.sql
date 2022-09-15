65afaf20f072936b8bbf8335e282c5c46b68e975
Miguel Angel Cabrera Minagorri <devgorri@gmail.com>
2022-09-15 18:50:53 +0200

    Rename triggers to avoid overlaps with other tools
    Signed-off-by: Miguel Angel Cabrera Minagorri <devgorri@gmail.com>

--- schema.sql
+++ schema.sql
@@ -12,19 +12,19 @@ CREATE TABLE rollback_monitor.status (
 INSERT INTO rollback_monitor.status(in_progress) VALUES (FALSE);
 
 -- Rollback starts when a block is deleted
-CREATE OR REPLACE FUNCTION start_rollback() RETURNS trigger as $$
+CREATE OR REPLACE FUNCTION rollback_monitor_start_rollback() RETURNS trigger as $$
 BEGIN
   UPDATE rollback_monitor.status SET in_progress=TRUE;
-  RETURN NEW;
+  RETURN OLD;
 EXCEPTION WHEN OTHERS THEN
   -- Prevent our schema to affect db-sync table updates if it fails
   RAISE WARNING 'There was an error setting rollback as started: %', sqlstate;
-  RETURN NEW;
+  RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
 
 -- Rollback stops when a block is added
-CREATE OR REPLACE FUNCTION stop_rollback() RETURNS trigger as $$
+CREATE OR REPLACE FUNCTION rollback_monitor_stop_rollback() RETURNS trigger as $$
 BEGIN
   IF ((SELECT in_progress FROM rollback_monitor.status) = TRUE) THEN
     UPDATE rollback_monitor.status SET in_progress=FALSE;
@@ -39,11 +39,11 @@ END;
 $$ LANGUAGE plpgsql;
 
 /* Trigger to handle the start of a rollback */
-CREATE OR REPLACE TRIGGER block_rollback BEFORE DELETE ON block
+CREATE OR REPLACE TRIGGER rollback_monitor_block_rollback BEFORE DELETE ON block
     FOR EACH ROW
-    EXECUTE PROCEDURE start_rollback();
+    EXECUTE PROCEDURE rollback_monitor_start_rollback();
 
 /* Trigger to handle the end of a rollback */
-CREATE OR REPLACE TRIGGER new_block BEFORE INSERT ON block
+CREATE OR REPLACE TRIGGER rollback_monitor_new_block BEFORE INSERT ON block
     FOR EACH ROW
-    EXECUTE PROCEDURE stop_rollback();
+    EXECUTE PROCEDURE rollback_monitor_stop_rollback();
